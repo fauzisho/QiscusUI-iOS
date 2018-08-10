2234c5b76995c6f2f42755232394eac6fbe897da
ariefnurputranto <arief@qiscus.co>
2018-08-10 11:20:26 +0700

    change ui default for chatList

--- QiscusUI/View/ChatList/UIChatListViewCell.swift
+++ QiscusUI/View/ChatList/UIChatListViewCell.swift
@@ -20,11 +20,14 @@ class UIChatListViewCell: UITableViewCell {
         return String(describing: self)
     }
     
+    @IBOutlet weak var viewBadge: UIView!
+    @IBOutlet weak var imageViewPinRoom: UIImageView!
     @IBOutlet weak var labelName: UILabel!
     @IBOutlet weak var labelLastMessage: UILabel!
     @IBOutlet weak var imageViewRoom: UIImageView!
     @IBOutlet weak var labelDate: UILabel!
     
+    @IBOutlet weak var labelBadge: UILabel!
     var data : RoomModel? {
         didSet {
             if data != nil {
@@ -33,9 +36,40 @@ class UIChatListViewCell: UITableViewCell {
         }
     }
     
+    var lastMessageCreateAt:String{
+        get{
+            var createAt = data?.lastComment?.unixTimestamp
+            if createAt == 0 {
+                return ""
+            }else{
+                var result = ""
+                let date = Date(timeIntervalSince1970: Double(createAt!))
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "d/MM"
+                let dateString = dateFormatter.string(from: date)
+                
+                let timeFormatter = DateFormatter()
+                timeFormatter.dateFormat = "h:mm a"
+                let timeString = timeFormatter.string(from: date)
+                
+                if date.isToday{
+                    result = "\(timeString)"
+                }
+                else if date.isYesterday{
+                    result = "Yesterday"
+                }else{
+                    result = "\(dateString)"
+                }
+                
+                return result
+            }
+        }
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        imageViewRoom.layer.cornerRadius = imageViewRoom.frame.height/2
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -47,10 +81,32 @@ class UIChatListViewCell: UITableViewCell {
     private func setupUI() {
         if let data = data {
             self.labelName.text = data.name
-//            self.labelDate.text = data.lastComment?.time
-            self.labelLastMessage.text  = data.lastComment?.message
+            self.labelDate.text = lastMessageCreateAt
+            if(data.chatType != "single"){
+                self.labelLastMessage.text  =  "\((data.lastComment?.username)!): \((data.lastComment?.message)!)"
+            }else{
+                 self.labelLastMessage.text  = data.lastComment?.message
+            }
+           
             self.imageViewRoom.af_setImage(withURL: URL.init(string: data.avatarUrl)!)
+            if(data.unreadCount == 0){
+                self.hiddenBadge()
+            }else{
+                self.showBadge()
+                self.labelBadge.text = "\(data.unreadCount)"
+            }
+            
         }
+    }
+    
+    public func hiddenBadge(){
+        self.viewBadge.isHidden     = true
+        self.labelBadge.isHidden    = true
+    }
+    
+    public func showBadge(){
+        self.viewBadge.isHidden     = false
+        self.labelBadge.isHidden    = false
     }
     
 }
