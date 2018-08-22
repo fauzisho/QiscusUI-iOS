18359f025f4dd338391099e36f45506578699638
asharijuang <asharijuang@me.com>
2018-08-22 19:45:21 +0700

    fix 1st time load blank

--- QiscusUI/Controllers/Chat List/UIChatListPresenter.swift
+++ QiscusUI/Controllers/Chat List/UIChatListPresenter.swift
@@ -39,7 +39,7 @@ class UIChatListPresenter {
     
     private func loadFromLocal() {
         // get from local
-        self.rooms = QiscusCore.storage.getRooms()
+        self.rooms = QiscusCore.dataStore.getRooms()
         if self.rooms.count > 0 {
             self.viewPresenter?.didFinishLoadChat(rooms: self.rooms)
         }
@@ -49,8 +49,12 @@ class UIChatListPresenter {
         // check update from server
         QiscusCore.shared.getAllRoom(limit: 50, page: 1) { (rooms, meta, error) in
             if let results = rooms {
+                // check 1st time load
+                if self.rooms.count == 0 && results.count > 0 {
+                    self.rooms = results
+                }
                 self.viewPresenter?.didFinishLoadChat(rooms: results)
-                self.rooms = results
+                self.rooms = results // 2nd load rooms
             }else {
                 self.viewPresenter?.setEmptyData(message: "")
             }
@@ -67,7 +71,7 @@ extension UIChatListPresenter : QiscusCoreDelegate {
         // show in app notification
         print("got new comment: \(comment.message)")
         self.viewPresenter?.updateRooms(data: room)
-        self.rooms = QiscusCore.storage.getRooms()
+        self.rooms = QiscusCore.dataStore.getRooms()
         // MARK: TODO receive new comment, need trotle
         QiscusCore.shared.updateCommentReceive(roomId: room.id, lastCommentReceivedId: comment.id)
     }
