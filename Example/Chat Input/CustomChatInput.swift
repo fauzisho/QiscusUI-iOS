c245f055ea71dd84cbf07b2158920a93022cbba3
Ashari Juang <asharijuang@me.com>
2018-10-11 12:19:12 +0700

    propose sample set heigh

--- Example/Chat Input/CustomChatInput.swift
+++ Example/Chat Input/CustomChatInput.swift
@@ -38,7 +38,7 @@ class CustomChatInput: UIChatInput {
     
     @IBAction func clickAttachment(_ sender: Any) {
         self.delegate?.sendAttachment()
-        self.setHeight(self.frame.height)
+        self.setHeight(self.frame.height + 50)
     }
 }
 
