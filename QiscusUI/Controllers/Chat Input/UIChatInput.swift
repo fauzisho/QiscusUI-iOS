5c8825847fe8547b7e9818c693d95fb3d79786a2
Ashari Juang <asharijuang@me.com>
2018-10-11 11:28:02 +0700

    Revert "InputView adaptive lines based on the UITextView"

--- QiscusUI/Controllers/Chat Input/UIChatInput.swift
+++ QiscusUI/Controllers/Chat Input/UIChatInput.swift
@@ -22,10 +22,9 @@ protocol UIChatInputDelegate {
 
 open class UIChatInput: UIView {
 
-    @IBOutlet weak var tvInput: UITextView?
     @IBOutlet weak var btnAttachment: UIButton!
     @IBOutlet weak var btnSend: UIButton!
-
+    @IBOutlet weak var tfInput: UITextField!
     var delegate : UIChatInputDelegate? {
         set {
             self._delegate = newValue
@@ -35,15 +34,7 @@ open class UIChatInput: UIView {
         }
     }
     private var _delegate       : UIChatInputDelegate? = nil
-    private var inputToBeCalculated: UITextView?
-    private var inputViewMaxLines: CGFloat = 4
     var contentsView            : UIView!
-    var onHeightChange: (CGFloat) -> Void = { _ in }
-    open override var frame: CGRect {
-        didSet {
-            onHeightChange(frame.height)
-        }
-    }
 
     // If someone is to initialize a UIChatInput in code
     public override init(frame: CGRect) {
@@ -76,70 +67,17 @@ open class UIChatInput: UIView {
         contentsView.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
         
         self.autoresizingMask  = (UIViewAutoresizing.flexibleWidth)
-        if let input = self.tvInput {
-            input.delegate = self
-        }
-    }
-    
-    // set maxLines based on external UITextView (sorry my shortcut to create documentation doesn't work, i don't know why)
-    open func setInputViewMaxLines(with lines: CGFloat, basedOn view: UITextView) {
-        self.inputViewMaxLines = lines
-        self.inputToBeCalculated = view
     }
     
     @IBAction private func clickUISendButton(_ sender: Any) {
-        guard let text = self.tvInput?.text else {return}
+        guard let text = self.tfInput.text else {return}
         if !text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
             let message = CommentModel()
             message.message = text
             message.type    = "text"
             self._delegate?.send(message: message)
         }
-        self.tvInput?.text = ""
-    }
-    
-    private func calculateHeight() -> CGRect {
-        
-        if let customInputView = self.inputToBeCalculated {
-            // do calculation based on custom view by client app
-            return self.getInputHeight(inputView: customInputView)
-        } else {
-            // do calculation based default input view
-            if let inputView = tvInput {
-                return self.getInputHeight(inputView: inputView)
-            }
-        }
-        
-        return CGRect()
-    }
-    
-    private func getInputHeight(inputView: UITextView) -> CGRect {
-        var tvInputFrame = inputView.frame
-        tvInputFrame.size.height = inputView.contentSize.height + 2
-        
-        var inputContainerFrame = self.frame
-        inputContainerFrame.size.height = inputView.contentSize.height + 10
-        return inputContainerFrame
-    }
-}
-
-extension UIChatInput : UITextViewDelegate {
-    open func textViewDidChange(_ textView: UITextView) {
-        let fontHeight = textView.font?.lineHeight
-        let line = textView.contentSize.height / fontHeight!
-        
-        if line < inputViewMaxLines {
-            self.frame = self.calculateHeight()
-            self.layoutIfNeeded()
-        }
-    }
-    
-    open func textViewDidBeginEditing(_ textView: UITextView) {
-        self._delegate?.typing(true)
-    }
-    
-    open func textViewDidEndEditing(_ textView: UITextView) {
-        self._delegate?.typing(false)
+        self.tfInput.text = ""
     }
 }
 
