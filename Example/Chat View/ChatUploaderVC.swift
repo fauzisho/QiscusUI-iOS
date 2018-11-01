705ee150db2157eb498ae91a975937468ed3c996
ariefnurputranto <arief@qiscus.co>
2018-11-01 11:18:11 +0700

    implement chatUploader

--- Example/Chat View/ChatUploaderVC.swift
+++ Example/Chat View/ChatUploaderVC.swift
@@ -7,13 +7,106 @@
 //
 
 import UIKit
+import Photos
+import MobileCoreServices
+import QiscusCore
+import QiscusUI
 
-class ChatUploaderVC: UIViewController {
+enum ChatUploaderType {
+    case image
+    case video
+}
 
+class ChatUploaderVC: UIViewController,UIScrollViewDelegate,UITextViewDelegate {
+    @IBOutlet weak var labelUploading: UILabel!
+    @IBOutlet weak var heightProgressViewCons: NSLayoutConstraint!
+    @IBOutlet weak var labelProgress: UILabel!
+    @IBOutlet weak var progressView: UIView!
+    @IBOutlet weak var containerProgressView: UIView!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var deleteButton: UIButton!
+    @IBOutlet weak var inputBottom: NSLayoutConstraint!
+    @IBOutlet weak var mediaCaption: UITextView!
+    @IBOutlet weak var minInputHeight: NSLayoutConstraint!
+    @IBOutlet weak var mediaBottomMargin: NSLayoutConstraint!
+    
+    var chatView:ChatViewController?
+    var type = ChatUploaderType.image
+    var data   : Data?
+    var fileName :String?
+    var imageData: [CommentModel] = []
+    var selectedImageIndex: Int = 0
+    let maxProgressHeight:Double = 40.0
+    var maxUploadSizeInKB:Double = Double(100) * Double(1024)
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let keyboardToolBar = UIToolbar()
+        keyboardToolBar.sizeToFit()
+        
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem:
+            UIBarButtonSystemItem.flexibleSpace, target: nil, action: nil)
+        let doneButton = UIBarButtonItem(barButtonSystemItem:
+            UIBarButtonSystemItem.done, target: self, action: #selector(self.doneClicked) )
+        
+        keyboardToolBar.setItems([flexibleSpace, doneButton], animated: true)
+        
+        mediaCaption.inputAccessoryView = keyboardToolBar
+        
+        mediaCaption.text = "Caption here"
+        mediaCaption.textColor = UIColor.lightGray
+        mediaCaption.delegate = self
+        
+        self.autoHideKeyboard()
+        self.scrollView.delegate = self
+        self.scrollView.minimumZoomScale = 1.0
+        self.scrollView.maximumZoomScale = 4.0
+        let sendImage = UIImage(named: "send")
+        self.sendButton.setImage(sendImage, for: .normal)
+        self.cancelButton.setTitle("Cancel", for: .normal)
+        
+        self.deleteButton.isHidden = true
+        self.sendButton.isEnabled = false
+        self.sendButton.isHidden = true
+        if self.fileName != nil && self.data != nil && self.imageData.count == 0 {
+            QiscusCore.shared.upload(data: data!, filename: fileName!, onSuccess: { (file) in
+                self.sendButton.isEnabled = true
+                self.sendButton.isHidden = false
+                self.labelUploading.isHidden = true
+                let message = CommentModel()
+                message.type = "file_attachment"
+                message.payload = [
+                    "url"       : file.url.absoluteString,
+                    "file_name" : file.name,
+                    "size"      : file.size,
+                    "caption"   : ""
+                ]
+                message.message = "Send Attachment"
+                self.imageData.append(message)
+            }, onError: { (error) in
+                //
+            }) { (progress) in
+                
+                self.labelProgress.text = "\(Int(progress * 100)) %"
+                self.labelProgress.isHidden = false
+                self.containerProgressView.isHidden = false
+                self.progressView.isHidden = false
+                
+                let newHeight = progress * self.maxProgressHeight
+                self.heightProgressViewCons.constant = CGFloat(newHeight)
+                UIView.animate(withDuration: 0.65, animations: {
+                    self.progressView.layoutIfNeeded()
+                })
+            }
+            
+        }
+        
+        for gesture in self.view.gestureRecognizers! {
+            self.view.removeGestureRecognizer(gesture)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,14 +115,106 @@ class ChatUploaderVC: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @objc func doneClicked() {
+        view.endEditing(true)
     }
-    */
-
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.textColor == UIColor.lightGray {
+            textView.text = nil
+            textView.textColor = UIColor.black
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = "Caption here"
+            textView.textColor = UIColor.lightGray
+        }
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if self.data != nil {
+            if type == .image {
+                self.imageView.image = UIImage(data: self.data!)
+            }
+        }
+        
+        let center: NotificationCenter = NotificationCenter.default
+        center.addObserver(self, selector: #selector(ChatUploaderVC.keyboardWillHide(_:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        center.addObserver(self, selector: #selector(ChatUploaderVC.keyboardChange(_:)), name: NSNotification.Name.UIKeyboardWillChangeFrame, object: nil)
+        self.navigationController?.isNavigationBarHidden = true
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillChangeFrame, object: nil)
+        self.navigationController?.isNavigationBarHidden = false
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return self.imageView
+    }
+    
+    @IBAction func sendMedia(_ sender: Any) {
+        if type == .image {
+            if (mediaCaption.text != "Caption here" ){
+                self.imageData.first?.payload![ "caption" ] = mediaCaption.text
+            }
+            
+            chatView?.send(message: self.imageData.first!, onSuccess: { (comment) in
+                let _ = self.navigationController?.popViewController(animated: true)
+            }, onError: { (error) in
+                let _ = self.navigationController?.popViewController(animated: true)
+            })
+        }
+    }
+    
+    // MARK: - Keyboard Methode
+    @objc func keyboardWillHide(_ notification: Notification){
+        let info: NSDictionary = (notification as NSNotification).userInfo! as NSDictionary
+        
+        let animateDuration = info[UIKeyboardAnimationDurationUserInfoKey] as! Double
+        self.inputBottom.constant = 0
+        self.mediaBottomMargin.constant = 8
+        UIView.animate(withDuration: animateDuration, delay: 0, options: UIViewAnimationOptions(), animations: {
+            self.view.layoutIfNeeded()
+            
+        }, completion: nil)
+    }
+    @objc func keyboardChange(_ notification: Notification){
+        let info:NSDictionary = (notification as NSNotification).userInfo! as NSDictionary
+        let keyboardSize = (info[UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        
+        let keyboardHeight: CGFloat = keyboardSize.height
+        let animateDuration = info[UIKeyboardAnimationDurationUserInfoKey] as! Double
+        
+        self.inputBottom.constant = keyboardHeight
+        self.mediaBottomMargin.constant = -(self.mediaCaption.frame.height + 8)
+        UIView.animate(withDuration: animateDuration, delay: 0, options: UIViewAnimationOptions(), animations: {
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+    }
+    
+    @IBAction func cancel(_ sender: Any) {
+        let _ = self.navigationController?.popViewController(animated: true)
+    }
 }
+
+extension UIViewController {
+    
+    func autoHideKeyboard() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+}
+
+
