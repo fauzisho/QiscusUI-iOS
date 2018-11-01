72cbf2283cd483bae9a267c12d045255a0ecb922
ariefnurputranto <arief@qiscus.co>
2018-11-01 11:16:38 +0700

    add file chatUploader

---
+++ Example/Chat View/ChatUploaderVC.swift
@@ -0,0 +1,35 @@
+//
+//  ChatUploaderVC.swift
+//  Example
+//
+//  Created by asharijuang on 01/11/18.
+//  Copyright © 2018 Qiscus. All rights reserved.
+//
+
+import UIKit
+
+class ChatUploaderVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
